5ca7dfd8b9a51afeb4a1ae83447db8896c06921c
Agustina <agustina_1312@hotmail.com>
2025-01-16 13:44:20 -0300

    Agregué mi cv

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,21 @@ const About = () => {
   Cursando la Tecnicatura en Programación  en La Universidad Tecnológica Nacional. Experiencia en lenguajes:  Java, C, HTML, CSS, React y JavaScript. Mi principal interés se concentra  en el desarrollo front-end, buscando  constantemente aprender nuevas tecnologías.
   </p>
   <div style={{ display: "flex", justifyContent: "center" }}>
-    <button id="btn-cv">Ver CV</button>
+    <a
+      href="/CV.pdf" // Ruta del archivo PDF en la carpeta public
+      target="_blank" // Abre el archivo en una nueva pestaña
+      rel="noopener noreferrer" // Buena práctica para enlaces externos
+      id="btn-cv"
+      style={{
+        padding: "10px 20px",
+        color: "white",
+        backgroundColor: "#f04ab8",
+        textDecoration: "none",
+        borderRadius: "5px",
+      }}
+    >
+      Ver CV
+    </a>
   </div>
 </section>
 
